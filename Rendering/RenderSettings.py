07adce83b9e39fbc4407d2167a3f09a429afa964
Someone-42 <tristn.lemarchand@gmail.com>
2022-01-20 14:58:07 +0100

    Render settings class

---
+++ Rendering/RenderSettings.py
@@ -0,0 +1,15 @@
+import Rendering.graphics as g
+
+class RenderSettings:
+    __slots__=(
+        "clear_color",
+        "vial_per_row",
+        "margin_size",
+        "vial_spacing"
+        )
+    def __init__(self, vial_per_row = 6, margin_size = 0.15, vial_spacing = 0.7, clear_color=g.couleur(0, 0, 0)):
+        """ Creates a class with rendering settings for the renderer to use - margin_size has to be in screen space (from 0 to 1) - vial_spacing is a fraction of a vial_size.x """
+        self.clear_color = clear_color
+        self.vial_per_row = vial_per_row
+        self.margin_size = margin_size
+        self.vial_spacing = vial_spacing
