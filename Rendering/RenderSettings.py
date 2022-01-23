c58e1f96cdd6295f81ab05bbd4c57e73314d7428
Someone-42 <tristn.lemarchand@gmail.com>
2022-01-23 09:06:25 +0100

    Settings use Color object

--- Rendering/RenderSettings.py
+++ Rendering/RenderSettings.py
@@ -1,4 +1,4 @@
-import Rendering.graphics as g
+import Rendering.Core as core
 
 class RenderSettings:
     __slots__=(
@@ -7,7 +7,7 @@ class RenderSettings:
         "margin_size",
         "vial_spacing"
         )
-    def __init__(self, vial_per_row = 6, margin_size = 0.15, vial_spacing = 0.7, clear_color=g.couleur(0, 0, 0)):
+    def __init__(self, vial_per_row = 6, margin_size = 0.15, vial_spacing = 0.7, clear_color=core.Color(0, 0, 0)):
         """ Creates a class with rendering settings for the renderer to use - margin_size has to be in screen space (from 0 to 1) - vial_spacing is a fraction of a vial_size.x """
         self.clear_color = clear_color
         self.vial_per_row = vial_per_row
