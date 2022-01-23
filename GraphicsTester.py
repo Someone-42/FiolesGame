ca03dc01a1147eb68333dbf7f9248efe56530ce0
Someone-42 <tristn.lemarchand@gmail.com>
2022-01-23 09:07:22 +0100

    UI Uses r.load method -> new module to test rendering

---
+++ GraphicsTester.py
@@ -0,0 +1,4 @@
+import UI as ui
+
+if __name__ == "__main__":
+    ui.start(1200, 900, "A testing thingy")
--- UI.py
+++ UI.py
@@ -1,10 +1,13 @@
 import Rendering.Renderer as r
+import Rendering.RenderSettings as rs
+import Rendering.Core as core
 
 def start(width, height, title = "A window"):
     r.init()
+    r.load(rs.RenderSettings(6, 0.15, 0.7, core.Color(42, 42, 42)))
     r.create_window(width, height, title)
 
-def display_game(game: Game):
+def display_game(game):
     r.clear()
     r.render_vials(game.vials)
     r.render()
