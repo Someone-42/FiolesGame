9b55bf3d8047953adc8cfe2ddc2f366a9ffa6316
Someone-42 <tristn.lemarchand@gmail.com>
2022-01-27 17:31:19 +0100

    ui_rect is a defined attribute now.....

--- Rendering/RenderSettings.py
+++ Rendering/RenderSettings.py
@@ -6,7 +6,8 @@ class RenderSettings:
         "vial_per_row",
         "margin_size",
         "vial_spacing",
-        "row_spacing"
+        "row_spacing",
+        "ui_rect"
         )
     def __init__(self, vial_per_row = 6, margin_size = 0.05, vial_spacing = 0.6, row_spacing = 0.5, ui_rect = core.Rectangle2(Vector2(0, 0), Vector2(1, 0.15)), clear_color=core.Color(0, 0, 0)):
         """ Creates a class with rendering settings for the renderer to use - margin_size has to be in screen space (from 0 to 1) - vial_spacing is a fraction of a vial_size.x """
@@ -15,3 +16,4 @@ class RenderSettings:
         self.margin_size = margin_size
         self.vial_spacing = vial_spacing
         self.row_spacing = row_spacing
+        self.ui_rect = ui_rect
