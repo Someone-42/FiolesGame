3e6650f56480ea37bbf4ed9811046b9bc56a21bc
Megai'Sa <95303095+MegaiSa@users.noreply.github.com>
2022-01-29 15:57:13 +0100

    Fixing problems

--- fixed_stack.py
+++ fixed_stack.py
@@ -8,13 +8,14 @@ class Fixedstack:
     def push(self, element):
         """push new element in the stack"""
         assert self.capacity > self.top_element + 1, "stack overflow. Maximum capacity of stack exceeded (Max " + str(self.capacity) + " ) "
-        self.item[self.top_element] = element
+        self.item[self.top_element + 1] = element
         self.top_element += 1
 
     def pop(self):
         """pop top element of the stack"""
         assert self.top_element >= 0, "you cant pop an empty list"
         value = self.item[self.top_element] 
+        self.item[self.top_element] = 0
         self.top_element -= 1
         return value
 
@@ -23,3 +24,9 @@ class Fixedstack:
         assert self.top_element >= 0, "There is no element in this stack"
         return self.item[self.top_element]
 
+if __name__ == "__main__":
+    test = Fixedstack(3)
+    test.push(7)
+    print(test.item)
+    test.pop()
+    print(test.item)
