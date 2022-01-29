4f163addd926c1e311eb3296dc6ce2a9396d4744
Someone-42 <tristn.lemarchand@gmail.com>
2022-01-29 15:57:30 +0100

    Renames to convention

--- FixedStack.py
+++ FixedStack.py
@@ -1,25 +1,25 @@
-class Fixedstack:
+class FixedStack:
 
     def __init__(self, capacity):
         self.capacity = capacity
-        self.item = [0] * capacity
+        self.items = [0] * capacity
         self.top_element = -1
 
     def push(self, element):
         """push new element in the stack"""
         assert self.capacity > self.top_element + 1, "stack overflow. Maximum capacity of stack exceeded (Max " + str(self.capacity) + " ) "
-        self.item[self.top_element] = element
+        self.items[self.top_element] = element
         self.top_element += 1
 
     def pop(self):
         """pop top element of the stack"""
         assert self.top_element >= 0, "you cant pop an empty list"
-        value = self.item[self.top_element] 
+        value = self.items[self.top_element] 
         self.top_element -= 1
         return value
 
     def peek(self):
         "peek the top element in the stack"
         assert self.top_element >= 0, "There is no element in this stack"
-        return self.item[self.top_element]
+        return self.items[self.top_element]
 
