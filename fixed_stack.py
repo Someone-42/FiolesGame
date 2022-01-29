f404297441d86706ad672d55cfb950302e85c3a4
Megai'Sa <95303095+MegaiSa@users.noreply.github.com>
2022-01-29 14:45:23 +0100

    Adds push module

--- fixed_stack.py
+++ fixed_stack.py
@@ -5,5 +5,10 @@ class Fixedstack:
         self.item = [0] * capacity
         self.top_element = -1
 
+    def push(self, element):
+        """push new element in the stack"""
+        assert self.capacity > self.top_element + 1, "stack overflow. Maximum capacity of stack exceeded (Max " + str(self.capacity) + " ) "
+        self.item[self.top_element] = element
+        self.top_element += 1
 
 
