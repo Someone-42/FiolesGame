bb2971595d4721411e7ce1003106ef6572803c4d
AYAYA <95303032+Nox612@users.noreply.github.com>
2022-02-01 17:22:44 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -4,3 +4,12 @@ class Game:
     def __init__(self):
         self.vials = []
         self.moves = Stack()
+        
+    def try_moving(self, a, b):
+        if self.vials[a]:
+            if len(self.vials[b]) != 3:
+                return True
+        return False
+
+    def is_finished(self):
+        pass
