664989ddaab9a18c319698551f2e8465ea56de78
AYAYA <95303032+Nox612@users.noreply.github.com>
2022-02-02 17:59:18 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,10 +4,10 @@ from UI import*
 game = Game()
 
 def run():
-    start(1920,1080,"glass") #glass gamiiiiing
+    start(1920,1080,"glass")
     while not game.finished():
         display_game(game)
         (i1, i2) = wait_for_move()
         if not game.try_move(i1, i2):
-            print("couldn't move")
+            is_unable_to_move()
         
