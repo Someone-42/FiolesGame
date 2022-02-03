08c5b880f12a754f8ed2902be6354186de1d6350
Someone-42 <tristn.lemarchand@gmail.com>
2022-02-03 10:16:05 +0100

    Fixed vial indexes and name

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ game = Game()
 
 def run():
     start(1920,1080,"glass")
-    while not game.finished():
+    while not game.is_finished():
         display_game(game)
         (i1, i2) = wait_for_move()
         if not game.try_move(i1, i2):
