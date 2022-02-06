3851067153952eef2e6ed2d89a82c48f99c5657c
Someone-42 <tristn.lemarchand@gmail.com>
2022-02-06 09:58:48 +0100

    ModelBase for buttons

--- Rendering/BaseModels/ButtonModels.py
+++ Rendering/BaseModels/ButtonModels.py
@@ -1,14 +1,26 @@
 from Rendering.Core import *
 import Rendering.graphics as g
 
-class QuitButtonModel:
+class ButtonModel:
+    __slots__ = ("baked_rect")
+    def __init__(self):
+        self.baked_rect = None
+
+    def render(self, button):
+        pass
+
+    def set_baked_rect(self, rect):
+        self.baked_rect = rect
+
+class QuitButtonModel(ButtonModel):
     __slots__ = ("color")
     def __init__(self, color = (255, 0, 0)):
         self.color = color
+        super().__init__()
 
-    def render(self, button, rect):
+    def render(self, button):
         g.affiche_rectangle_plein(
-            rect.pos.to_tuple(),
-            (rect.pos + rect.size).to_tuple(),
+            self.baked_rect.pos.to_tuple(),
+            (self.baked_rect.pos + self.baked_rect.size).to_tuple(),
             self.color.to_tuple_rgb()
         )
