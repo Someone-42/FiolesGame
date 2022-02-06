4dc6102a6c0dff1d7b70b387a7d96050cef7ff5b
Someone-42 <tristn.lemarchand@gmail.com>
2022-02-06 09:50:22 +0100

    Rendersettings -> quit_button option

--- Rendering/RenderSettings.py
+++ Rendering/RenderSettings.py
@@ -1,5 +1,9 @@
 import Rendering.Core as core
 from Rendering.BaseModels.VialModel import VialModel
+from Rendering.BaseModels.ButtonModels import QuitButtonModel
+
+def _quit_button_return(self):
+    return (core.InputType.QUIT)
 
 class RenderSettings:
     __slots__=(
@@ -10,9 +14,21 @@ class RenderSettings:
         "row_spacing",
         "vial_rect",
         "ui_rect",
-        "vial_model"
+        "vial_model",
+        "quit_button"
         )
-    def __init__(self, vials_per_row = 6, margin_size = 0.025, vial_spacing = 0.7, row_spacing = 0.5, vial_rect = core.Rectangle2(core.Vector2(0, 0.15), core.Vector2(1, 0.85)), ui_rect = core.Rectangle2(core.Vector2(0, 0), core.Vector2(1, 0.15)), clear_color=core.Color(0, 0, 0), vial_model=VialModel()):
+    def __init__(self, vials_per_row = 6, 
+        margin_size = 0.025, 
+        vial_spacing = 0.7, 
+        row_spacing = 0.5, 
+        vial_rect = core.Rectangle2(core.Vector2(0, 0.15), 
+        core.Vector2(1, 0.85)), 
+        ui_rect = core.Rectangle2(core.Vector2(0, 0), 
+        core.Vector2(1, 0.15)), 
+        clear_color=core.Color(0, 0, 0), 
+        vial_model=VialModel(),
+        quit_button=core.Button(core.Rectangle2(core.Vector2(0.85, 0), core.Vector2(0.15, 0.15)), QuitButtonModel(), _quit_button_return)
+        ):
         """ Creates a class with rendering settings for the renderer to use - margin_size has to be in screen space (from 0 to 1) - vial_spacing is a fraction of a vial_size.x """
         self.clear_color = clear_color
         self.vials_per_row = vials_per_row
@@ -23,3 +39,4 @@ class RenderSettings:
         m_v = core.Vector2(margin_size, margin_size)
         self.vial_rect = core.Rectangle2(vial_rect.pos + m_v, vial_rect.size - (m_v * 2))
         self.vial_model = vial_model
+        self.quit_button = quit_button
