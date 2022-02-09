ff8bf3653419c74f99fd644fb2431049e4c04897
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2022-02-09 11:17:28 +0100

    Level example, Parser

---
+++ Parser.py
@@ -0,0 +1,22 @@
+class Parser:
+    def __init__(self, file_path):
+        pass
+
+    def save_level(self, vials: list) -> int:
+        """ Saves a level, returns its save index """
+        pass
+
+    def load_level(self, index: int) -> list:
+        """ Loads the level according to the index (list of vials)"""
+        # Open file
+        # index < len(file)
+        # Read to list
+        # Parse to list of vials
+        # Close
+        pass
+
+    def _read_to_list(self, level_string: str, index: int):
+        """ Reads a level in memory and returns it as lists """
+        # Read the file
+        # Return list of lists
+        pass
