e9dbc9f41e5d348d0065ddb3a5afd61c14f29e29
Megai'Sa <95303095+MegaiSa@users.noreply.github.com>
2022-02-10 22:21:27 +0100

    Finish read_to_list and load_level

--- Parser.py
+++ Parser.py
@@ -1,6 +1,8 @@
+import Vials
+
 class Parser:
     def __init__(self, file_path):
-        pass
+        self.file_path = file_path
 
     def save_level(self, vials: list) -> int:
         """ Saves a level, returns its save index """
@@ -8,15 +10,26 @@ class Parser:
 
     def load_level(self, index: int) -> list:
         """ Loads the level according to the index (list of vials)"""
-        # Open file
-        # index < len(file)
-        # Read to list
-        # Parse to list of vials
-        # Close
-        pass
+        file = open(self.file_path, "r")
+        lines = file.readlines()
+        file.close()
+        if index >= len(lines):            
+            raise IndexError("Out of range exception - level doesn't exist")
+        else:
+            level = self._read_to_list(lines[index])
+            level_vials = []
+            for colors in level:
+                vial = Vials.Vial(len(colors))
+                vial.push_content(colors)
+                level_vials.append(vial)
+            return level_vials
 
-    def _read_to_list(self, level_string: str, index: int):
+    def _read_to_list(self, level_string: str):
         """ Reads a level in memory and returns it as lists """
-        # Read the file
-        # Return list of lists
-        pass
+        vial_strings = level_string.split("|")
+        vials = [[int(color) for color in vial.split(" ")] for vial in vial_strings]
+        return vials
+
+if __name__ == "__main__":
+    test = Parser("Content/Levels1.txt")
+    print(test.load_level(0) )
